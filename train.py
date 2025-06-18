a4a86132cf4712cb2d33e6c37d4880f4346db95f
Fernand Pajot <fernand@epigram.me>
2025-06-18 13:25:26 -0600

    .

--- train.py
+++ train.py
@@ -198,14 +198,13 @@ def train():
             delta  = target - v_pred_t.detach()  # advantage, no grad
 
             # 1 Actor update (before critic)
+            actor_opt.zero_grad()
             mu, std, _ = net(obs_t)
             dist = TanhNormal(mu, std)
             act_t = torch.as_tensor(act, dtype=torch.float32, device=DEVICE)
             logp = dist.log_prob(act_t).sum()
             entropy = dist.normal.entropy().sum()
-            actor_opt.zero_grad()
             action_penalty = 0.001 * (act_t ** 2).sum()
-            actor_opt.zero_grad()
             (-logp - ENTROPY_COEF * entropy + action_penalty).backward()
             apply_traces(net.actor_params, actor_tr, scale=delta.detach())
             torch.nn.utils.clip_grad_norm_(net.actor_params, 0.5)

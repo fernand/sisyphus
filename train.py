09c4d4db9bb7513db0afbd73eba4279b1433ab0b
Fernand Pajot <fernand@epigram.me>
2025-06-18 13:35:40 -0600

    .

--- train.py
+++ train.py
@@ -25,22 +25,6 @@ MAX_STEPS    = 1600
 HIDDEN_SIZES = (128, 64)
 DEVICE       = torch.device('cpu')
 
-class RunningStats:
-    def __init__(self, eps=1e-8):
-        self.mean = 0.0
-        self.var = 1.0
-        self.count = eps
-
-    def update_single(self, x):
-        """Update with a single value"""
-        self.count += 1
-        delta = x - self.mean
-        self.mean += delta / self.count
-        self.var += delta * (x - self.mean - self.var) / self.count
-
-    def normalize(self, x):
-        return (x - self.mean) / np.sqrt(self.var + 1e-8)
-
 class ActorCritic(nn.Module):
     """Shared torso, Gaussian actor, scalar critic."""
 
@@ -151,7 +135,6 @@ def train():
     critic_tr = [torch.zeros_like(p) for p in net.critic_params]
 
     obs_norm = RunningNorm(obs_dim)
-    reward_stats = RunningStats()
     recent_returns = []
 
     for ep in trange(MAX_EPISODES, desc='episodes'):
@@ -185,12 +168,6 @@ def train():
             act, _ = net.act(obs)
             obs_next, r, term, trunc, _ = env.step(act)
             done = term or trunc
-
-            # Update stats with this single reward
-            reward_stats.update_single(r)
-
-            # Normalize the reward for use in TD update
-            r_normalized = reward_stats.normalize(r)
 
             # Cast scalars to tensors early
             r_t = torch.as_tensor(r_normalized, dtype=torch.float32, device=DEVICE)
